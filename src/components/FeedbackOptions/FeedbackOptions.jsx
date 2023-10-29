fb4f584dc52b1c4f364d9784e8161f979d4b2c44
Pe4enyffka <serhyd.89@gmail.com>
2023-10-29 12:00:02 +0100

    rev1

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,25 @@
+import css from './FeedbackOptions.module.css';
+import PropTypes from 'prop-types';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+    return (
+        <div className={css.list}>
+        {options.map(option => {
+            return (
+            <button type="button" onClick={() => onLeaveFeedback(option)}
+                key={option}
+                className={css.btn}>
+                {option}
+            </button>
+        );
+        })}
+        </div>         
+    );
+};
+
+FeedbackOptions.propTypes = {
+    options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+    onLeaveFeedback: PropTypes.func.isRequired,
+};
+
+export default FeedbackOptions;
